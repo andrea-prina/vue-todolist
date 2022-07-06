70d28ebfb075b783829ddf239ae1558908a43767
andrea-prina <and.prina@gmail.com>
2022-07-06 15:55:06 +0200

    Dinamicizzato inserimento task con vue

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 const app = new Vue(
     {
-        el : "#app"
+        el : "#app",
+
+        data : {
+            todos: [
+                {
+                    text: 'Fare i compiti',
+                    done: false
+                },
+                {
+                    text: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Fare il bucato',
+                    done: false
+                }
+            ]
+        }
     }
 )
